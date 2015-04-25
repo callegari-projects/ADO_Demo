aedd1127bb63df87f03aecfe38e016bd770d1eaf
Victor Callegari <callegari.projects@gmail.com>
2015-04-24 21:52:40 -0700

    First commit

---
+++ ADO_Demo/ADO_Demo/BAL/Student_BAL.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+
+namespace ADO_Demo.BAL
+{
+    public class Student_BAL
+    {
+
+        public static List<Student>  GetUserAll()
+        {
+
+            //List<Student> Student = new List<Student>();
+            
+            DataTable dt = new DataTable();
+
+            dt = SQLHelpers.SQLHelpers.GetTable("DemoConnection", "[dbo].[GetStudent_all]", null, true);
+
+
+            List<Student> studentList = new List<Student>();
+            for (int i = 0; i < dt.Rows.Count; i++)
+            {
+                Student student = new Student();
+
+                student.StudentId = dt.Rows[i]["StudentId"].ToString();
+                student.FirstName = dt.Rows[i]["FirstName"].ToString();
+                student.LastName = dt.Rows[i]["LastName"].ToString();
+                student.Age = (int)dt.Rows[i]["Age"];
+                student.Gender = dt.Rows[i]["Gender"].ToString();
+                student.Batch = dt.Rows[i]["Batch"].ToString();
+                student.Address = dt.Rows[i]["Address"].ToString();
+                student.Class = dt.Rows[i]["Class"].ToString();
+                student.School = dt.Rows[i]["School"].ToString();
+                student.Domicile = dt.Rows[i]["Domicile"].ToString();
+
+
+
+                studentList.Add(student);
+            } 
+
+
+            return studentList;
+
+        }
+
+    }
+}
---
+++ ADO_Demo/ADO_Demo/Student.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ADO_Demo
+{
+    public partial class Student
+    {
+
+        public string StudentId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public Nullable<int> Age { get; set; }
+        public string Gender { get; set; }
+        public string Batch { get; set; }
+        public string Address { get; set; }
+        public string Class { get; set; }
+        public string School { get; set; }
+        public string Domicile { get; set; }
+
+    }
+}
